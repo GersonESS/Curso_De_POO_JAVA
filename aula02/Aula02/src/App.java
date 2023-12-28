aeb53a589d7a7543b4eb599c26d90643b0785496
Gerson Santos <gersones@outlook.com>
2023-12-27 23:13:38 -0300

    a

--- aula02/Aula02/src/App.java
+++ aula02/Aula02/src/App.java
@@ -2,6 +2,7 @@
 public class App {
     public static void main(String[] args) {
         Caneta c1 = new Caneta();
+        c1.modelo = "Parquer";
         c1.cor = "Azul";
         c1.ponta = 0.5f;
         c1.tampar();
